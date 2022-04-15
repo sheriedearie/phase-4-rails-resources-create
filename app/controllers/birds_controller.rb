62c9e133cb29256f2fd5f23a3ed35aece77c47eb
sheridan <sheridanmaustad@gmail.com>
2022-04-15 11:02:11 -0600

    commit tree 3e5a2c6901a8f53d7f1ab9e10073b63cd90ff043

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,10 @@
 class BirdsController < ApplicationController
-
+  # POST /birds
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+    # byebug
+  end
   # GET /birds
   def index
     birds = Bird.all
@@ -15,5 +20,4 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
-
 end
